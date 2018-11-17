4538485ed7348626e7f342c28b21bab951bd9d9d
olegchursin <olegchursin@gmail.com>
2018-11-17 16:36:18 -0500

    New program

--- src/views/Registration.jsx
+++ src/views/Registration.jsx
@@ -11,21 +11,6 @@ const styles = {
   }
 };
 
-{
-  /* <iframe
-  onLoad={this.scrollToTop}
-  src="https://docs.google.com/forms/d/e/1FAIpQLScBieN1X779Ry2iNmavPMZaj_G7kTGt8YC5CzbOObFvzdNQEQ/viewform?embedded=true"
-  width="640"
-  height="1700"
-  frameBorder="0"
-  marginHeight="0"
-  marginWidth="0"
-  title="Registration form"
->
-  Loading...
-</iframe>; */
-}
-
 export class Registration extends Component {
   scrollToTop = () => {
     window.scroll({ top: 0, behavior: 'smooth' });
